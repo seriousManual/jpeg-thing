10daae5ef11a677302e159c1a373283c7628ac70
Manuel Ernst <seriousstuff@gmx.de>
2017-01-17 11:19:47 +0100

    refactor createMatrix

--- src/lib/createMatrix.js
+++ src/lib/createMatrix.js
@@ -8,13 +8,11 @@ export function createColorMatrix() {
     var g = parseInt(Math.random() * 256, 10)
     var b = parseInt(Math.random() * 256, 10)
 
-    m.forEach((x, y) => {
+    return m.map(_ => {
         r += (Math.random() < 0.5 ? -1 : 1) * (Math.random() * Math.random() * 100)
         g += (Math.random() < 0.5 ? -1 : 1) * (Math.random() * Math.random() * 100)
         b += (Math.random() < 0.5 ? -1 : 1) * (Math.random() * Math.random() * 100)
 
-        m.set(x, y, [colorClamp(r), colorClamp(g), colorClamp(b)])
-    })
-
-    return m
+        return [colorClamp(r), colorClamp(g), colorClamp(b)]
+    }, m)
 }
