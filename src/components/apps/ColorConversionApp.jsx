ee60922591d46814d8819dfbc0ee3d37e0d7ba59
Manuel Ernst <seriousstuff@gmx.de>
2017-01-17 07:58:39 +0100

    refactor color conversion app

--- src/components/apps/ColorConversionApp.jsx
+++ src/components/apps/ColorConversionApp.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-import MatrixBasedSubApp from '../MatrixBasedSubApp'
-
 import ColorMatrixVisualisation from '../matrix/visualisation/ColorMatrix'
 
 import {rgbToYCbCrMatrix, yCbCrToRgbMatrix} from '../../lib/colorConversion'
@@ -10,49 +8,19 @@ import {mergeTupelMatrices} from '../../lib/mergeTupelMatrices'
 import {subSample, overSample} from '../../lib/sampling'
 import {createColorMatrix} from '../../lib/createMatrix'
 
-class ColorConversionApp extends MatrixBasedSubApp {
-    _getReducer() {
-        return (state, action) => {
-            if (!state) {
-                state = updateState({baseMatrix: createColorMatrix()})
-            }
+class ColorConversionApp extends React.Component {
+    constructor() {
+        super()
 
-            if (action.type === 'NEW_MATRIX') {
-                var newMatrix = createColorMatrix()
-
-                return updateState({baseMatrix: newMatrix})
-            }
-
-            return state
-        }
-
-        function updateState(state) {
-            var yCbCrMatrix = rgbToYCbCrMatrix(state.baseMatrix)
-            var splitted = splitTupelMatrix(yCbCrMatrix, 3)
-
-            state.yMatrix = splitted[0]
-            state.CBMatrix = splitted[1]
-            state.CRMatrix = splitted[2]
-
-            state.CBMatrixSubSampled = subSample(splitted[1], 2)
-            state.CRMatrixSubSampled = subSample(splitted[2], 2)
-
-            state.CBMatrixReSampled = overSample(state.CBMatrixSubSampled, 2)
-            state.CRMatrixReSampled = overSample(state.CRMatrixSubSampled, 2)
-
-            state.resultMatrix = yCbCrToRgbMatrix(mergeTupelMatrices(state.yMatrix, state.CBMatrixReSampled, state.CRMatrixReSampled))
-
-            return state
-        }
+        this.state = this._createState()
     }
 
-
-    _render () {
-        var {baseMatrix, yMatrix, CBMatrix, CRMatrix, CBMatrixSubSampled, CRMatrixSubSampled, CBMatrixReSampled, CRMatrixReSampled, resultMatrix} = this._store.getState()
+    render() {
+        var {baseMatrix, yMatrix, CBMatrix, CRMatrix, CBMatrixSubSampled, CRMatrixSubSampled, CBMatrixReSampled, CRMatrixReSampled, resultMatrix} = this.state
 
         return (
             <div className="colorConversionsApp">
-                <span onClick={() => this._store.dispatch({type: 'NEW_MATRIX'})}>New Matrix</span>
+                <span onClick={() => this.setState(this._createState())}>New Matrix</span>
                 <ColorMatrixVisualisation matrix={baseMatrix}/>
                 <ColorMatrixVisualisation matrix={yMatrix}/>
                 <ColorMatrixVisualisation matrix={CBMatrix}/>
@@ -64,6 +32,29 @@ class ColorConversionApp extends MatrixBasedSubApp {
                 <ColorMatrixVisualisation matrix={resultMatrix}/>
             </div>
         )
+    }
+
+    _createState() {
+        return this._updateState({baseMatrix: createColorMatrix()})
+    }
+
+    _updateState(state) {
+        var yCbCrMatrix = rgbToYCbCrMatrix(state.baseMatrix)
+        var splitted = splitTupelMatrix(yCbCrMatrix, 3)
+
+        state.yMatrix = splitted[0]
+        state.CBMatrix = splitted[1]
+        state.CRMatrix = splitted[2]
+
+        state.CBMatrixSubSampled = subSample(splitted[1], 2)
+        state.CRMatrixSubSampled = subSample(splitted[2], 2)
+
+        state.CBMatrixReSampled = overSample(state.CBMatrixSubSampled, 2)
+        state.CRMatrixReSampled = overSample(state.CRMatrixSubSampled, 2)
+
+        state.resultMatrix = yCbCrToRgbMatrix(mergeTupelMatrices(state.yMatrix, state.CBMatrixReSampled, state.CRMatrixReSampled))
+
+        return state
     }
 }
 
